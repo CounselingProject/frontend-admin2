809fd3da9c0a5b3bcb5316956e5d3ed63a9d7fcc
sswum <l5450won@naver.com>
2024-09-20 14:10:08 +0900

    집단상담프로그램 오류 수정

--- src/app/counseling/group/page.js
+++ src/app/counseling/group/page.js
@@ -1,8 +1,11 @@
-const CounselingGroupPage = () => {
-
-return <h1>집단상담 프로그램 목록</h1>
-
-}
-
+import GroupListContainer from '@/counseling/containers/GroupListContainer';
+import AdminOnlyContainer from '@/member/containers/AdminOnlyContainer';
+const CounselingGroupPage = ({ searchParams }) => {
+  return (
+    <AdminOnlyContainer>
+      <GroupListContainer searchParams={searchParams} />
+    </AdminOnlyContainer>
+  );
+};
 
 export default CounselingGroupPage;
