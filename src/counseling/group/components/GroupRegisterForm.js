faf4f6436b1acfca47fd39f2b3c69e92abe9b037
sswum <l5450won@naver.com>
2024-09-20 14:28:27 +0900

    집단상담프로그램 등록 코드 수정중

--- src/counseling/group/components/GroupRegisterForm.js
+++ src/counseling/group/components/GroupRegisterForm.js
@@ -24,13 +24,77 @@ const GroupRegisterForm = ({ onSubmit, }) => {
 
   return <FormBox autoComplete="off" onSubmit={onSubmit}>
     <dl>
-      <dt>{t('집단상담프로그램명d')}</dt>
+      <dt>{t('집단상담 프로그램번호')}</dt>
       <dd>
-      <StyledInput>
-
-      </StyledInput>
+      <StyledInput
+      type="text"
+      name="no"
+      onChange={onChange}
+      />
       </dd>
     </dl>
+    <dl>
+      <dt>{t('집단상담 프로그램명')}</dt>
+      <dd>
+      <StyledInput
+      type="text"
+      name="name"
+      onChange={onChange}
+      />
+      </dd>
+    </dl>
+    <dl>
+      <dt>{t('집단상담 프로그램 설명')}</dt>
+      <dd>
+      <StyledInput
+      type="text"
+      name="des"
+      onChange={onChange}
+      />
+      </dd>
+    </dl>
+
+    <dl>
+      <dt>{t('상담사명')}</dt>
+      <dd>
+      <StyledInput
+      type="text"
+      name="cname"
+      onChange={onChange}
+      />
+      </dd>
+    </dl>
+    <dl>
+      <dt>{t('상담사 이메일')}</dt>
+      <dd>
+      <StyledInput
+      type="text"
+      name="email"
+      onChange={onChange}
+      />
+      </dd>
+    </dl>
+    <dl>
+      <dt>{t('집단상담 프로그램 신청 시작일')}</dt>
+      <dd>
+      <StyledInput
+      type="text"
+      name="sdate"
+      onChange={onChange}
+      />
+      </dd>
+    </dl>
+    <dl>
+      <dt>{t('집단상담 프로그램 신청 종료일')}</dt>
+      <dd>
+      <StyledInput
+      type="text"
+      name="edate"
+      onChange={onChange}
+      />
+      </dd>
+    </dl>
+
 
   </FormBox>;
 };
