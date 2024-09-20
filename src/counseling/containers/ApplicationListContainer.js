d0685c16bebf8188940a731d068f5b7ee1dec33f
jek730 <jejek730@gmail.com>
2024-09-20 19:19:38 +0900

    ApplicationListContainer, ItemBox 추가

--- src/counseling/containers/ApplicationListContainer.js
+++ src/counseling/containers/ApplicationListContainer.js
@@ -1,17 +1,80 @@
 'use client';
-import React, { useLayoutEffect } from 'react';
+import React, { useLayoutEffect, useEffect, useState, useCallback } from 'react';
 import { getCommonActions } from '@/commons/contexts/CommonContext';
+import { apiList } from '../../counseling/details/apis/apiInfo';
+import ItemsBox from '../../counseling/details/components/ItemsBox';
+import Pagination from '../../commons/components/Pagination';
+import { useTranslation } from 'react-i18next';
+
+function getQueryString(searchParams) {
+  const qs = {};
+  if (searchParams.size > 0) {
+    for (const [k, v] of searchParams) {
+      qs[k] = v;
+    }
+  }
+  return qs;
+}
 
 const ApplicationListContainer = ({ searchParams }) => {
   const { setMenuCode, setSubMenuCode } = getCommonActions();
+
+  const [search, setSearch] = useState(() => getQueryString(searchParams));
+  const [items, setItems] = useState([]);
+  const [pagination, setPagination] = useState({});
+  const { rNo } = useParams();
+
+  const { t } = useTranslation();
 
   useLayoutEffect(() => {
     setMenuCode('counseling');
     setSubMenuCode('apply');
   }, [setMenuCode, setSubMenuCode]);
 
+  useEffect(() => {
+    (async () => {
+      try {
+        const { items, pagination } = await apiList(search);
+        setItems(items);
+        setPagination(pagination);
+      } catch (err) {
+        console.error(err);
+      }
+    })();
+  }, [search, rNo]);
 
-  return <h1>상담 신청 목록</h1>;
+  const onChangePage = useCallback((p) => {
+    setSearch((search) => ({ ...search, page: p }));
+  }, []);
+
+  const onCancel = useCallback(
+    (orderNo) => {
+      if (!window.confirm(t('정말_취소하겠습니까'))) {
+        return;
+      }
+
+      (async () => {
+        try {
+          const res = await apiCancel(orderNo);
+          setItems((items) =>
+            items.map((item) => (item.orderNo === orderNo ? res : item)),
+          );
+        } catch (err) {
+          console.error(err);
+        }
+      })();
+    },
+    [t],
+  );
+
+  return (
+    <>
+      <ItemsBox items={items} onCancel={onCancel} />
+      {items.length > 0 && (
+        <Pagination onClick={onChangePage} pagination={pagination} />
+      )}
+    </>
+  );
 };
 
 
