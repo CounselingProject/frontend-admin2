bb6bf0040dc2df5356d37cc30c470910dd67768e
parksaehyun <psh666777@naver.com>
2024-09-23 12:35:43 +0900

    feat : 등록된 회원 리스트 조회 api, 컨테이너, 컴포넌트 구성중

--- src/member/apis/apiInfo.js
+++ src/member/apis/apiInfo.js
@@ -1,4 +1,46 @@
 import requestData from '@/commons/libs/requestData';
 
+// 교수 목록 키워드 검색
 export const getProfessors = (skey) =>
   requestData(`/member/account/professors?skey=${skey?.trim()}`);
+
+// 인증(로그인)한 회원 정보 조회
+export const getMemberInfo = () =>
+  requestData('/member/account', {
+    method: 'GET',
+  });
+
+// 회원정보 수정
+export const updateMemberInfo = async (form) => {
+  return requestData('/admin/update', {  // 경로를 '/admin/update'로 수정
+    method: 'PATCH',
+    body: JSON.stringify(form),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
+// 상담원 랜덤 조회
+export const getRandomCounselor = () =>
+  requestData('/member/account/counselor', {
+    method: 'GET',
+  });
+
+// 회원 목록 조회
+export const getMemberList = (page = 1, limit = 20, sopt = 'ALL', skey) =>
+  requestData(`/admin?page=${page}&limit=${limit}&sopt=${sopt}&skey=${skey?.trim()}`, {
+    method: 'GET',
+  });
+
+// 특정 회원 정보 조회 (이메일로)
+export const getMemberInfoByEmail = (email) =>
+  requestData(`/admin/info/${email}`, {
+    method: 'GET',
+  });
+
+// 회원 탈퇴
+export const deleteMember = (seq) =>
+  requestData(`/admin/delete/${seq}`, {
+    method: 'DELETE',
+  });
